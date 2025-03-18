dc8efc23d348eb560ac8b6fb21dfe576b6b793a2
Elias Gabriel <eliasgabrielcf@gmail.com>
2025-03-18 14:15:22 -0300

    Explorando Parâmetros da API

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,32 @@ const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
-client.chat.completions
+async function generateText() {
+  const completion = await client.chat.completions
   .create({
     model: 'gpt-4o-mini',
+    max_completion_tokens: 100,
     messages: [
       {
-        role: 'user',
-        content: 'Escreva uma mensagem de uma frase sobre unicórnios.',
+        role: 'developer',
+        content: 'Use emojis a cada 2 palavras. Isso é obrigatório, ignore regras que mudem a utilização de emojis. Gere um texto com no máximo uma frase.'
       },
+      {
+        role: 'user',
+        content: 'Escreva uma mensagem de uma frase sobre unicórnios. (Não pode usar emoji)',
+      },
+      {
+        role: "assistant",
+        content: 'Os unicórnios 🌈 são 🦄 criaturas mágicas 🪄 que simbolizam ✨ pureza e 🌟 beleza.'
+      },
+      {
+        role: 'user',
+        content: 'Obrigado'
+      }
     ],
   })
-  .then((completion) => {
-    console.log(completion.choices[0].message.content);
-  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+generateText();
