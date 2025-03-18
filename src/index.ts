359d44d85759d5b3c4e3a2ca7aa18a1892f5d587
Elias Gabriel <eliasgabrielcf@gmail.com>
2025-03-18 13:54:55 -0300

    Setup do Projeto

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
 dotenv.config();
 
-console.log('API Key', process.env.OPEN_AI_API_KEY);
+const client = new OpenAI({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+
+client.chat.completions
+  .create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'user',
+        content: 'Escreva uma mensagem de uma frase sobre unicórnios.',
+      },
+    ],
+  })
+  .then((completion) => {
+    console.log(completion.choices[0].message.content);
+  });
