f66e63fac4d1e1db7e73c514884c764795c94275
Elias Gabriel <eliasgabrielcf@gmail.com>
2025-05-29 13:25:16 -0300

    Setup do Carrinho

--- database/dump.sql
+++ database/dump.sql
@@ -16,6 +16,35 @@ CREATE TABLE products (
     embedding VECTOR(1536) -- Adjust the dimension based on your embedding model
 );
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE carts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    store_id INTEGER REFERENCES stores(id),
+    active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE cart_items (
+    id SERIAL PRIMARY KEY,
+    cart_id INTEGER REFERENCES carts(id),
+    product_id INTEGER REFERENCES products(id),
+    quantity INTEGER NOT NULL DEFAULT 1,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_cart_product UNIQUE (cart_id, product_id)
+);
+
+-- Usuários
+INSERT INTO users (name, email, password) VALUES
+  ('John Doe', 'johndoe@email.com', 'dummyhash');
+
 -- Lojas
 INSERT INTO stores (name) VALUES
   ('Supermercado Central'),
