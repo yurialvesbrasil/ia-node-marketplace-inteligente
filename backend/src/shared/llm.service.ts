262e0fc16489772e9c8fb2cbaed8f874b5026585
Elias Gabriel <eliasgabrielcf@gmail.com>
2025-07-03 17:55:20 -0300

    Batch Embedding Parte 1

--- backend/src/shared/llm.service.ts
+++ backend/src/shared/llm.service.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import OpenAI from 'openai';
 import z from 'zod';
 import { zodTextFormat } from 'openai/helpers/zod';
+import { CreateEmbeddingResponse } from 'openai/resources/embeddings';
 
 const answerMessageSchema = z.object({
   message: z.string(),
@@ -42,6 +43,89 @@ export class LlmService {
       apiKey: this.configService.get<string>('OPEN_AI_API_KEY'),
       webhookSecret: this.configService.get<string>('OPEN_AI_WEBHOOK_SECRET'),
     });
+  }
+
+  async batchEmbedProducts(products: { id: number; name: string }[]) {
+    const jsonlFile = products
+      .map((product) =>
+        JSON.stringify({
+          custom_id: product.id.toString(),
+          method: 'POST',
+          url: '/v1/embeddings',
+          body: {
+            model: 'text-embedding-3-small',
+            input: product.name,
+          },
+        }),
+      )
+      .join('\n');
+
+    const uploadedFile = await this.client.files.create({
+      file: new File([jsonlFile], 'products.jsonl', {
+        type: 'application/jsonl',
+      }),
+      purpose: 'batch',
+    });
+
+    if (!uploadedFile.id) {
+      console.error('Failed to upload file for batch embedding');
+      return null;
+    }
+
+    await this.client.batches.create({
+      input_file_id: uploadedFile.id,
+      completion_window: '24h',
+      endpoint: '/v1/embeddings',
+    });
+  }
+
+  async handleWebhookEvent(rawBody: string, headers: Record<string, string>) {
+    console.log('LlmService.handleWebhookEvent called');
+    const event = await this.client.webhooks.unwrap(rawBody, headers);
+
+    if (event.type !== 'batch.completed') {
+      console.warn('Received non-batch event:', event.type);
+      return;
+    }
+
+    console.log('Batch completed event received:', event.data.id);
+    const batch = await this.client.batches.retrieve(event.data.id);
+    if (!batch || !batch.output_file_id) {
+      console.error('Batch output file not found:', event.data.id);
+      return;
+    }
+
+    console.log('Batch output file ID:', batch.output_file_id);
+    const outputFile = await this.client.files.content(batch.output_file_id);
+    const results = (await outputFile.text())
+      .split('\n')
+      .filter((line) => line.trim() !== '')
+      .map((line) => {
+        const data = JSON.parse(line) as {
+          custom_id: string;
+          response: {
+            body: CreateEmbeddingResponse;
+          };
+        };
+
+        if (
+          !data.response ||
+          !data.response.body ||
+          !data.response.body.data ||
+          data.response.body.data.length === 0
+        ) {
+          console.warn('Invalid response data:', data);
+          return null;
+        }
+
+        return {
+          productId: data.custom_id,
+          embedding: data.response.body.data[0].embedding,
+        };
+      })
+      .filter((result) => result !== null);
+
+    return results;
   }
 
   async embedInput(input: string): Promise<{ embedding: number[] } | null> {
